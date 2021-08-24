95b94c7a01c040cee268cac0f74adefba7615b61
fridabrodin <frida.brodin@hotmail.com>
2021-08-24 09:43:26 +0200

    Added common

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import { handleSubmit } from './js/formHandler'
 import './styles/style.scss'
-import "../media/background.jpg";
+import "/src/media/background.jpg";
 
 //Set value for travel date as the date of today
 document.getElementById("travel-date").valueAsDate = new Date();
